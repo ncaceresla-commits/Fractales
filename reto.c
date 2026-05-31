4c81363ecfa38e1d3d38fc34535c25e3557c52dc
ncaceresla-commits <ncaceresla@unsa.edu.pe>
2026-05-31 09:13:23 -0500

    Para agregar una tercera rama en el centro, dividi a la mitad los angulos para dibujar la rama central

---
+++ reto.c
@@ -0,0 +1,47 @@
+#include "turtlec.h"
+
+void modTree(Turtle *t, float length, int depth){
+        if(depth == 0 )
+                return ;
+        turtleForward(t, length);
+        if (depth > 3)
+                turtleSetColor(t, 120, 70, 20);
+        else
+                turtleSetColor(t, 0,200 , 0);
+
+        turtleLeft(t, 30.0f);
+        modTree(t, length * 0.7, depth - 1);
+
+	turtleRight(t, 30.0f);
+	modTree(t, length * 0.7, depth -1);
+
+        turtleRight(t, 30.0f);
+        modTree(t, length * 0.7, depth - 1);
+
+        turtleLeft(t, 30.0f);
+
+        turtlePenUp(t);
+        turtleBackward(t, length);
+        turtlePenDown(t);
+}
+
+int main(){
+        TurtleApp *app = turtleAppCreate(800, 600, "arbol fractal");
+
+        if(app == NULL)
+                return 1;
+        Turtle *t = turtleAppGetTurtle(app);
+
+        turtlePenUp(t);
+        turtleGoTo(t, 400.0f, 300.0f);
+        turtlePenDown(t);
+
+        turtleSetColor(t,120,70,20);
+	turtleSetSpeed(t, 10.0f);
+        modTree(t,100.0f,5);
+
+        turtleAppRun(app);
+        turtleAppDestroy(app);
+
+         return 0;
+}
